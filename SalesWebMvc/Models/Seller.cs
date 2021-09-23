5f59673711e42fd4e4b50ffda945b49c7e8baab3
felipebortolin <felipebortolin@gmail.com>
2021-09-23 01:51:27 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -22,15 +22,22 @@ namespace SalesWebMvc.Models
         }
 
         public int Id { get; set; }
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60,MinimumLength =3, ErrorMessage ="{0} size should be between {2} and {1}")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        [Required(ErrorMessage = "{0} required")]
         public string Email { get; set; }
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "{0} required")]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
         [Display(Name = "Base Salary")]
+        [Required(ErrorMessage = "{0} required")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
 
